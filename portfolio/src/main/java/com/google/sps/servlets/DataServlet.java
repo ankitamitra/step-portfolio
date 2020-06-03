56857833fa665e2beb24a968359b30cd06968d37
Ankita Mitra <ankitamitra@berkeley.edu>
2020-06-03 06:09:12 +0000

    store comments as entities

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -14,8 +14,12 @@
 
 package com.google.sps.servlets;
 
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
 import com.google.gson.Gson;
 import java.io.IOException;
+import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.Arrays;
 import javax.servlet.annotation.WebServlet;
@@ -31,7 +35,7 @@ public class DataServlet extends HttpServlet {
   private ArrayList<String> messages = new ArrayList<String>(
       Arrays.asList("No comments")
   );
-
+  
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
     
@@ -57,16 +61,31 @@ public class DataServlet extends HttpServlet {
     String email = getParameter(request, "email", "");
     String subject = getParameter(request, "subject", "");
     String comments = getParameter(request, "comments", "");
+    String timestamp_UTC = new Timestamp(System.currentTimeMillis()).toString();
     
     messages = new ArrayList<String>();
     messages.add(name);
     messages.add(email);
     messages.add(subject);
     messages.add(comments);
+    messages.add(timestamp_UTC);
 
     // Respond with the result.
     response.setContentType("text/html;");
     response.getWriter().println(convertToJson(messages));
+
+    // Store comments.
+    Entity commentEntity = new Entity("Comment");
+    commentEntity.setProperty("timestamp_UTC", timestamp_UTC);
+    commentEntity.setProperty("name", name);
+    commentEntity.setProperty("email", email);
+    commentEntity.setProperty("subject", subject);
+    commentEntity.setProperty("comments", comments);
+
+    DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+    datastore.put(commentEntity);
+
+    response.sendRedirect("/index.html");
   }
 
   /**
