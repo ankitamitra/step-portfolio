36ee9d90a7ac9dcf9f40006dc3059afa07d9a2b9
Ankita Mitra <ankitamitra@berkeley.edu>
2020-06-09 19:14:16 +0000

    working autovalue for Restaurant class

--- portfolio/src/main/java/com/google/sps/data/Restaurant.java
+++ portfolio/src/main/java/com/google/sps/data/Restaurant.java
@@ -32,27 +32,3 @@ public abstract class Restaurant {
   abstract String cuisine();
   abstract String price();
 }
-
-// /** Represents a Michelin star restaurant at a specific lat lng point. */
-// public class Restaurant {
-//   private double lat;
-//   private double lng;
-//   private int star;
-//   private String name;
-//   private String city;
-//   private String region;
-//   private String cuisine;
-//   private String price;
-
-//   public Restaurant(double lat, double lng, int star, String name,
-//                     String city, String region, String cuisine, String price) {
-//     this.lat = lat;
-//     this.lng = lng;
-//     this.star = star;
-//     this.name = name;
-//     this.city = city;
-//     this.region = region;
-//     this.cuisine = cuisine;
-//     this.price = price;
-//   }
-// }
