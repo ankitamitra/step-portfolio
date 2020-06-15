156263733fa1c9df7e6ead7c6696857f486f4857
Ankita Mitra <ankitamitra@berkeley.edu>
2020-06-15 17:15:56 +0000

    removed dead code

--- portfolio/src/main/java/com/google/sps/servlets/RestaurantDataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/RestaurantDataServlet.java
@@ -64,7 +64,6 @@ public class RestaurantDataServlet extends HttpServlet {
       String price = cells[8];
 
       if (region.compareTo(my_region) == 0) {
-        //   restaurants.add(new Restaurant(lat, lng, star, name, city, region, cuisine, price));
         Restaurant restaurant =
             Restaurant.create(lat, lng, star, name, city, region, cuisine, price);
         restaurants.add(restaurant);
